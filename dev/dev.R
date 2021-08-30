2be0a95dc9a5a6738e72050dee5516552e222954
Oceane Cassan <oceane.cssn@gmail.com>
2021-08-30 09:22:36 +0200

    cairo

--- dev/dev.R
+++ dev/dev.R
@@ -3,7 +3,8 @@ usethis::use_package("stringr")
 usethis::use_package("igraph")
 usethis::use_package("ggraph")
 usethis::use_package("forcats")
-
+usethis::use_package("ggplot2")
+usethis::use_package("Cairo")
 
 
 usethis::use_vignette("Using-AraNetBench")
